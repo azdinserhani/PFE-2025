04fdfd19de1f2ffc2e294c03b9ebd384b7831295
azdine <azdineserhani1@gmail.com>
2025-03-10 23:44:00 +0000

    rename user table to user_acount and update foreign key references

--- Query.sql
+++ Query.sql
@@ -1,5 +1,5 @@
-CREATE TABLE user(
-    id PRIMARY KEY NOT NULL SERIAL,
+CREATE TABLE user_acount(
+    id SERIAL PRIMARY KEY NOT NULL ,
     username VARCHAR(20) NOT NULL UNIQUE,
     email VARCHAR(20) NOT NULL UNIQUE,
     password_hash  VARCHAR(255) NOT NULL,
@@ -13,12 +13,12 @@ CREATE TABLE enrolment(
     course_id INT NOT NULL,
     user_id INT NOT NULL,
     enrolled_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    completed_date TIMESTAMP
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(id),
+    completed_date TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user_acount(id),
     CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course(id)
 )
 CREATE TABLE course(
-    id SERIAL PRIMARY KEY NOT NULL ,
+    id SERIAL PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL UNIQUE,
     description TEXT NOT NULL,
     instructor_id INT NOT NULL,
@@ -26,25 +26,23 @@ CREATE TABLE course(
     category_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
-    CONSTRAINT fk_user FOREIGN KEY (instructor_id) REFERENCES user(id),
+    CONSTRAINT fk_user FOREIGN KEY (instructor_id) REFERENCES user_acount(id),
     CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category(id)
-)
+);
 
 CREATE TABLE category(
-    id PRIMARY KEY NOT NULL SERIAL,
-    course_id INT NOT NULL,
-    name VARCHAR(50) NOT NULL.
-    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course(id)
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL
 )
 CREATE TABLE module(
-    id PRIMARY KEY NOT NULL SERIAL,
+    id SERIAL PRIMARY KEY NOT NULL,
     course_id INT NOT NULL,
     name VARCHAR(50) NOT NULL,
     number  INT NOT NULL,
     CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course(id)
 )
 CREATE TABLE quiz(
-    id PRIMARY KEY NOT NULL SERIAL,
+    id SERIAL PRIMARY KEY NOT NULL ,
     course_id INT NOT NULL,
     name VARCHAR(50) NOT NULL,
     number INT NOT NULL,
@@ -52,14 +50,14 @@ CREATE TABLE quiz(
     CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course(id)
 )
 CREATE TABLE quiz_question(
-    id PRIMARY KEY NOT NULL SERIAL,
+    id SERIAL PRIMARY KEY NOT NULL ,
     quiz_id INT NOT NULL,
     question_title VARCHAR(255) NOT NULL,
     CONSTRAINT fk_quiz FOREIGN KEY (quiz_id) REFERENCES quiz(id)
 )
 
 CREATE TABLE quiz_answer(
-    id PRIMARY KEY NOT NULL SERIAL,
+    id SERIAL PRIMARY KEY NOT NULL ,
     question_id INT NOT NULL,
     answer_text TEXT NOT NULL,
     is_correct BOOLEAN NOT NULL,
@@ -68,26 +66,26 @@ CREATE TABLE quiz_answer(
 
 CREATE Table lesson(
     id SERIAL  PRIMARY KEY NOT NULL,
-    module_id int PRIMARY KEY NOT NULL,
+    module_id INT NOT NULL,
     name varchar(255) NOT NULL,
     number int NOT NULL,
     video_url VARCHAR(255) NOT NULL,
     lessons_details VARCHAR(255) NOT NULL,
-    is_free BOOLEAN
+    is_free BOOLEAN NOT NULL,
     CONSTRAINT fk_module FOREIGN KEY (module_id) REFERENCES module(id)
 )
 CREATE TABLE student_quiz_attempt (
     id SERIAL PRIMARY KEY NOT NULL,
-    student_id  int PRIMARY KEY NOT NULL,
-    quiz_id int PRIMARY KEY NOT NULL,
+    student_id  INT NOT NULL,
+    quiz_id INT NOT NULL,
     score_archived int NOT NULL,
-    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES user(id)
+    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES user_acount(id)
 )
 CREATE TABLE student_lesson(
     id SERIAL PRIMARY KEY NOT NULL,
     student_id int NOT NULL,
     lesson_id int NOT NULL,
     is_completed BOOLEAN,
-    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES user(id),
+    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES user_acount(id),
     CONSTRAINT fk_lesson FOREIGN KEY (lesson_id) REFERENCES lesson(id)
 )
