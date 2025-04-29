9c5763b42a1d7c0aaf006e65ce8d991fba6d90a3
azdine <azdineserhani1@gmail.com>
2025-04-29 14:05:30 +0100

    Implement course update and delete functionalities; enhance course retrieval methods

--- backend/course-service/controllers/courseController.js
+++ backend/course-service/controllers/courseController.js
@@ -3,8 +3,6 @@ import courseService from "../services/courseService.js";
 const courseController = {
   createCourse: async (req, res) => {
     try {
-      console.log("Creating course with data:", req.body);
-
       const courseData = req.body;
       const createdCourse = await courseService.createCourse(courseData);
       res.status(201).json({
@@ -16,6 +14,49 @@ const courseController = {
       const status = error.status || 500;
       const message =
         error.message || "An error occurred during crating the course";
+
+      res.status(status).json({
+        success: false,
+        message,
+      });
+    }
+  },
+  updateCourse: async (req, res) => {
+    try {
+      const courseId = req.params.id;
+      const courseData = req.body;
+      const updatedCourse = await courseService.updateCourse(
+        courseId,
+        courseData
+      );
+      res.status(200).json({
+        success: true,
+        message: "Course updated successfully",
+        data: updatedCourse,
+      });
+    } catch (error) {
+      const status = error.status || 500;
+      const message =
+        error.message || "An error occurred during retrieving the courses";
+
+      res.status(status).json({
+        success: false,
+        message,
+      });
+    }
+  },
+  deleteCourse: async (req, res) => {
+    try {
+      const courseId = req.params.id;
+      await courseService.deleteCourse(courseId);
+      res.status(200).json({
+        success: true,
+        message: "Course deleted successfully",
+      });
+    } catch (error) {
+      const status = error.status || 500;
+      const message =
+        error.message || "An error occurred during deleting the course";
 
       res.status(status).json({
         success: false,
@@ -36,6 +77,133 @@ const courseController = {
       const status = error.status || 500;
       const message =
         error.message || "An error occurred during retrieving the courses";
+
+      res.status(status).json({
+        success: false,
+        message,
+      });
+    }
+  },
+  getCourseById: async (req, res) => {
+    try {
+      const courseId = req.params.id;
+      const course = await courseService.getCourseById(courseId);
+      res.status(200).json({
+        success: true,
+        message: "Course retrieved successfully",
+        data: course,
+      });
+    } catch (error) {
+      const status = error.status || 500;
+      const message =
+        error.message || "An error occurred during retrieving the course";
+
+      res.status(status).json({
+        success: false,
+        message,
+      });
+    }
+  },
+  searchCourseByTitle: async (req, res) => { 
+    try {
+      const title = req.query.title;
+      const courses = await courseService.searchCourseByTitle(title);
+      res.status(200).json({
+        success: true,
+        message: "Courses retrieved successfully",
+        result: courses.length,
+        data: courses,
+      });
+    } catch (error) {
+      const status = error.status || 500;
+      const message =
+        error.message || "An error occurred during retrieving the courses";
+
+      res.status(status).json({
+        success: false,
+        message,
+      });
+    }
+  },
+  getCoursesByCategory: async (req, res) => { 
+    try {
+      const categoryId = req.params.category_id;
+      const courses = await courseService.getCoursesByCategory(categoryId);
+      res.status(200).json({
+        success: true,
+        message: "Courses retrieved successfully",
+        result: courses.length,
+        data: courses,
+      });
+    } catch (error) {
+      const status = error.status || 500;
+      const message =
+        error.message || "An error occurred during retrieving the courses";
+
+      res.status(status).json({
+        success: false,
+        message,
+      });
+    }
+  },
+  getCourseByInstructor: async (req, res) => {
+    try {
+      const instructorId = req.params.instructor_id;
+      const courses = await courseService.getCourseByInstructor(instructorId);
+      res.status(200).json({
+        success: true,
+        message: "Courses retrieved successfully",
+        result: courses.length,
+        data: courses,
+      });
+    } catch (error) {
+      const status = error.status || 500;
+      const message =
+        error.message || "An error occurred during retrieving the courses";
+
+      res.status(status).json({
+        success: false,
+        message,
+      });
+    }
+  },
+  getCourseContentByCourseId: async (req, res) => { 
+    try {
+      const courseId = req.params.id;
+      const courseContent = await courseService.getCourseContentByCourseId(
+        courseId
+      );
+      res.status(200).json({
+        success: true,
+        message: "Course content retrieved successfully",
+        data: courseContent,
+      });
+    } catch (error) {
+      const status = error.status || 500;
+      const message =
+        error.message || "An error occurred during retrieving the course";
+
+      res.status(status).json({
+        success: false,
+        message,
+      });
+    }
+  },
+  getEnrollStudentsByCourseId: async (req, res) => {
+    try {
+      const courseId = req.params.id;
+      const enrollStudents = await courseService.getEnrollStudentsByCourseId(
+        courseId
+      );
+      res.status(200).json({
+        success: true,
+        message: "Enroll students retrieved successfully",
+        data: enrollStudents,
+      });
+    } catch (error) {
+      const status = error.status || 500;
+      const message =
+        error.message || "An error occurred during retrieving the students";
 
       res.status(status).json({
         success: false,
--- backend/course-service/routes/courseRoute.js
+++ backend/course-service/routes/courseRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import courseController from "../controllers/courseController.js";
 import validateRequest from "../middleware/validateRequest.js";
 import { courseSchema } from "../validators/createCourseValidators.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post(
@@ -9,6 +10,29 @@ router.post(
   validateRequest(courseSchema),
   courseController.createCourse
 );
+router.patch(
+  "/course/:id",
+  validateRequest(courseSchema),
+  courseController.updateCourse
+);
+router.delete("/course/:id", courseController.deleteCourse);
 router.get("/courses", courseController.getAllCourses);
-
+router.get("/course/:id", courseController.getCourseById);
+router.get("/courses/search", courseController.searchCourseByTitle);
+router.get(
+  "/courses/category/:category_id",
+  courseController.getCoursesByCategory
+);
+router.get(
+  "/courses/instructor/:instructor_id",
+  courseController.getCourseByInstructor
+);
+router.get(
+  "/course/content/:course_id",
+  courseController.getCourseContentByCourseId
+);
+router.get(
+  "/course/enroll-students/:course_id",
+  courseController.getEnrollStudentsByCourseId
+);
 export default router;
